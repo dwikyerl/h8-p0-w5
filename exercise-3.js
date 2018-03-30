33ccbdf9ddfdb99c32304dd8a375bd70a96aa658
Dwiky Erlangga <dwikyerl@gmail.com>
2018-03-30 07:58:03 +0700

    Finished exercise 3 : Highest Score

---
+++ exercise-3.js
@@ -0,0 +1,89 @@
+/*
+  Logic Challenge: Highest Score
+*/
+
+function highestScore (students) {
+  let output = {};
+  if (students.length === 0) return output; 
+
+  for (let student of students) {
+    if (!output[student.class]) {
+      let report = {};
+      report.name = student.name;
+      report.score = student.score;
+      output[student.class] = report;
+    } else if (student.score > output[student.class].score) {
+      output[student.class].name = student.name;
+      output[student.class].score = student.score;
+    }
+  }
+
+  return output;
+}
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+
+console.log(highestScore([])); //{}
