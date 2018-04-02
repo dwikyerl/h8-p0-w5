3ed8875204f0c23125e4ecf9e8f97d18eb681461
Dwiky Erlangga <dwikyerl@gmail.com>
2018-04-02 07:55:20 +0700

    Finished exercise 9 : Cari Pelaku - Regex

---
+++ exercise-9.js
@@ -0,0 +1,15 @@
+/*
+  Logic Challenge - Cari Pelaku (Regex)
+*/
+
+function cariPelaku(str) {
+  const matched = str.match(/abc/g);
+  return matched.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
