04f84216472ae7ef0bfc0a3d7709ba2da33f3aaf
Dwiky Erlangga <dwikyerl@gmail.com>
2018-04-06 20:42:52 +0700

    Update exercise 2 : Naik Angkot to use Airbnb coding standard

--- exercise-2.js
+++ exercise-2.js
@@ -3,30 +3,32 @@
 */
 
 function naikAngkot(arrPenumpang) {
-  const rute = ["A", "B", "C", "D", "E", "F"];
-  let output = [];
+  const rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const output = [];
 
-  for (let passenger of arrPenumpang) {
+  arrPenumpang.forEach(passenger => {
     if (passenger.length === 3) {
-      let passengerObj = {};
-      let from = rute.indexOf(passenger[1]);
-      let to = rute.indexOf(passenger[2]);
-      let distance = to - from;
-      let cost = Math.abs(2000 * distance);
-      passengerObj["penumpang"] = passenger[0];
-      passengerObj["naikDari"] = passenger[1];
-      passengerObj["tujuan"] = passenger[2];
-      passengerObj["bayar"] = cost;
+      const passengerObj = {};
+      const from = rute.indexOf(passenger[1]);
+      const to = rute.indexOf(passenger[2]);
+      const distance = to - from;
+      const cost = Math.abs(2000 * distance);
+      [passengerObj.penumpang, passengerObj.naikDari, passengerObj.tujuan] = passenger;
+      passengerObj.bayar = cost;
       output.push(passengerObj);
     }
-  }
+  });
 
   return output;
 }
 
-//TEST CASE
-console.log(naikAngkot([["Dimitri", "B", "F"], ["Icha", "A", "B"]]));
+// TEST CASE
+console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
 // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
 //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
 
-console.log(naikAngkot([])); //[]
+console.log(naikAngkot([['Aloy', 'C', 'A'], ['Nathan', 'F', 'B']]));
+// [ { penumpang: 'Aloy', naikDari: 'C', tujuan: 'A', bayar: 4000 },
+//   { penumpang: 'Nathan', naikDari: 'F', tujuan: 'B', bayar: 8000 } ]
+
+console.log(naikAngkot([])); // []
